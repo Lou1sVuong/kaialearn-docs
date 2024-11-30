928999465aa240646de04d5a9e1660ad2d2eb399
lou1s vuong <lou1svuong.dev@gmail.com>
2024-11-30 14:56:28 +0700

    Add theme configuration for Kaialearn documentation

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Kaialearn Docs</span>,
+  project: {
+    link: "https://github.com/Lou1sVuong/kaialearn-docs",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+  docsRepositoryBase: "https://github.com/Lou1sVuong/kaialearn-docs/tree/main",
+  footer: {
+    text: "Nextra Docs Template",
+  },
+};
+
+export default config;
